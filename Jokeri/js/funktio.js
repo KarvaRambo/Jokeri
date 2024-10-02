cf95fd748e861e84bbf2cef6005a48e58b151488
Mikael Tuominen <143936503+KarvaRambo@users.noreply.github.com>
2024-10-02 13:44:33 +0300

    Add files via upload

---
+++ Jokeri/js/funktio.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('button')
+const table = document.querySelector('table')
+const pElement = document.querySelector('p')
+
+button.addEventListener('click', addNewRow)
+
+let rowCount = 0
+
+function addNewRow() {
+    const newRow = document.createElement('tr')
+    for (let i = 0; i < 7; i++) {
+        const newCell = document.createElement('td')
+        newCell.textContent = Math.floor(Math.random() * 10)
+        newRow.appendChild(newCell)
+    }
+
+    table.appendChild(newRow)
+    rowCount++;
+    pElement.textContent = `Valmiita rivejä: ${rowCount}`;
+}
